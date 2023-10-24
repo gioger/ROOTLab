8b921430bba171faca562740be3230d4db7cc6a2
Giorgio Germano <giorgio.germano.ravina@gmail.com>
2023-10-24 18:38:59 +0200

    implemented print. Task 1 is completed

--- particle.cpp
+++ particle.cpp
@@ -5,10 +5,17 @@
 ParticleType::ParticleType(std::string name, double mass, int charge)
     : m_name{std::move(name)}, m_mass{mass}, m_charge{charge}
 {
-    if (m_mass < 0.0)
+    if (m_mass < 0.)
     {
         std::cerr << "ParticleType::ParticleType: "
                   << "invalid mass value: " << m_mass << '\n';
         std::exit(EXIT_FAILURE);
     }
+}
+
+void ParticleType::Print() const
+{
+    std::cout << "Particle type: " << m_name << '\n'
+              << "Mass: " << m_mass << " MeV/c^2\n"
+              << "Charge: " << m_charge << " e\n";
 }

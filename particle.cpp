06acff33a5984b8ec85a01c48cb30e0c2dfe9b21
Marta Barbieri <marta.barbi@gmail.com>
2023-10-25 17:57:21 +0200

    Finishing 3 and starting 4

---
+++ particle.cpp
@@ -0,0 +1,12 @@
+#include "particle.hpp"
+
+#include <algorithm>
+
+int Particle::FindParticle(const std::string& particleName) {
+    auto it{std::find_if(fParticleType.begin(), fParticleType.end(), [](auto p){return p->GetName()==particleName;})};
+    if(it==fParticleType.end()){
+        std::cerr << "Particle type not found.\n";}
+        else {
+    return std::distance(fParticleType.begin(), it); //check if 0 or 1
+}
+}
--- particle.hpp
+++ particle.hpp
@@ -1,12 +1,25 @@
+#ifndef PARTICLE_TYPE_HPP
+#define
+
 #include <array>
 #include <iostream>
 #include <memory>
 
 class Particle {
     public:
-
+    Particle Particle(std::string name, double px, double py, double pz) : fName{name}, fPx{px}, fPy{py}, fpx{pz} {};
+    Particle Particle(std::string name) : Particle(name, 0, 0, 0);
 
     private:
     static constexpr int fMaxNumParticleType = 10;
+    static int fNParticleType{};
+    int fIndex{};
     static std::array<std::unique_ptr<ParticleType>, fMaxNumParticleType> fParticleType{};
+    int FindParticle(std::string nomeParticella);
+
+    double fPx{};
+    double fPy{};
+    double fPz{};
 };
+
+#endif

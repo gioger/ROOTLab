31a937f00a7c7fd3b94152fcdde1f60e3d5ed07b
Marta Barbieri <marta.barbi@gmail.com>
2023-11-02 16:53:12 +0100

    fixing run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ files="particle_type.cpp resonance_type.cpp particle.cpp main.cpp"
 
 g++ $files $(root-config --glibs --cflags --libs) -Wall -Wextra -o ../build/ROOTLab -O3
 
-cd build
+cd ../build
 
 echo "Running..."
 

8118e299464dfbc44e5e3e3dc7721e60e08ac0ca
Riccardo Franchi <richi.franchi2004@gmail.com>
2023-11-02 16:40:46 +0100

    First try... 🤞🤞🤞🤞

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ cd src
 
 files="particle_type.cpp resonance_type.cpp particle.cpp main.cpp"
 
-g++ $files $(root-config --glibs --cflags --libs) -Wall -Wextra -o ../build/ROOTLab
+g++ $files $(root-config --glibs --cflags --libs) -Wall -Wextra -o ../build/ROOTLab -O3
 
 cd ..
+
+echo "Running..."
 
 ./build/ROOTLab
